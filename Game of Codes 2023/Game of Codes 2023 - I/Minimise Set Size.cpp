7d05875ecb45bc14b9f5e83cab6f1d15725a1c62
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-05-10 01:11:02 +0530

    Update Minimise Set Size.cpp

--- Game of Codes 2023/Game of Codes 2023 - I/Minimise Set Size.cpp
+++ Game of Codes 2023/Game of Codes 2023 - I/Minimise Set Size.cpp
@@ -1,3 +1,34 @@
+/*You are given 2 numbers num and k. Return the size of the smallest set of positive integers (each having unit digit as k) such that sum of those numbers is equal to num.
+
+Note: Set may contain same number multiple times. Set with size 0 has sum 0.
+
+Input Format
+
+2 space separated integers num and k
+
+Output Format
+
+Return the integer, minimum size of set or return -1 if no such set is possible
+
+Sample Input 0
+
+58 9
+Sample Output 0
+
+2
+Explanation 0
+
+One valid set is [9, 49]. Another valid set is [19, 39]. It can be proved that 2 is the minimum size.
+
+Sample Input 1
+
+37 2
+Sample Output 1
+
+-1
+Explanation 1
+
+No valid set can be formed since it is not possible to form numbers that add up to 37 with 2 as the units digit.*/
 #include <bits/stdc++.h>
 using namespace std;
 
